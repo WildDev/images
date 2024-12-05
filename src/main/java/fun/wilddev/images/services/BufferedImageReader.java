fab5d31c26d4633ff711fd62a95f426e9ba79537
WildDev <georgy@wilddev.email>
2024-12-05 20:21:33 +0300

    Fixed the memory leak

--- src/main/java/fun/wilddev/images/services/BufferedImageReader.java
+++ src/main/java/fun/wilddev/images/services/BufferedImageReader.java
@@ -3,20 +3,21 @@ package fun.wilddev.images.services;
 import fun.wilddev.images.exceptions.images.buffered.BufferedImageReadingException;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import javax.imageio.ImageIO;
 
 import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 @Service
 public class BufferedImageReader {
 
     public BufferedImage read(@NonNull GridFsResource resource) throws BufferedImageReadingException {
 
-        try {
-            return ImageIO.read(resource.getInputStream());
+        try (InputStream is = resource.getInputStream()) {
+            return ImageIO.read(is);
         } catch (Exception ex) {
             throw new BufferedImageReadingException(ex);
         }
