4a78fde3469ce9e7c36b2112d63c44169813b9e1
WildDev <georgy@wilddev.email>
2025-11-23 10:25:11 +0300

    Extra index deleted

--- src/main/java/fun/wilddev/images/entities/webhooks/meta/ImageReference.java
+++ src/main/java/fun/wilddev/images/entities/webhooks/meta/ImageReference.java
@@ -1,7 +1,5 @@
 package fun.wilddev.images.entities.webhooks.meta;
 
-import org.springframework.data.mongodb.core.index.Indexed;
-
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.*;
 
@@ -12,7 +10,6 @@ import org.springframework.data.mongodb.core.mapping.*;
 @ToString
 public class ImageReference implements WebhookMeta {
 
-    @Indexed
     @Field(name = "image_id", targetType = FieldType.OBJECT_ID)
     private String imageId;
 }
